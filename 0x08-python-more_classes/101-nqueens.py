fd0b7463eeddaff7c3c09ceaf688cd4a1b2ef4d8
davyokeyo <daveokeyo97@gmail.com>
2022-06-28 15:59:05 -0700

    fix error

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -3,10 +3,7 @@
 nqueens backtracking program to print the coordinates of n queens
 on an nxn grid such that they are all in non-attacking positions
 """
-
-
 from sys import argv
-
 if __name__ == "__main__":
     a = []
     if len(argv) != 2:
@@ -19,18 +16,15 @@ if __name__ == "__main__":
     if n < 4:
         print("N must be at least 4")
         exit(1)
-
     # initialize the answer list
     for i in range(n):
         a.append([i, None])
-
     def already_exists(y):
         """check that a queen does not already exist in that y value"""
         for x in range(n):
             if y == a[x][1]:
                 return True
         return False
-
     def reject(x, y):
         """determines whether or not to reject the solution"""
         if (already_exists(y)):
@@ -41,12 +35,10 @@ if __name__ == "__main__":
                 return False
             i += 1
         return True
-
     def clear_a(x):
         """clears the answers from the point of failure on"""
         for i in range(x, n):
             a[i][1] = None
-
     def nqueens(x):
         """recursive backtracking function to find the solution"""
         for y in range(n):
@@ -57,6 +49,5 @@ if __name__ == "__main__":
                     print(a)
                 else:
                     nqueens(x + 1)  # moves on to next x value to continue
-
     # start the recursive process at x = 0
     nqueens(0)
